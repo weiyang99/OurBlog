43478564f821072cc7e992c5f5576c942755bfcc
weiyang <weiyangwygoh@gmail.com>
2023-07-05 19:49:07 +0800

    feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,13 +4,14 @@ import { useState, useEffect } from "react";
 
 import PostCard from "./PostCard";
 
-const PromptCardList = ({ data }) => {
+const PromptCardList = ({ data, handleTagClick }) => {
     return (
         <div className='post_layout'>
             {data.map((post) => (
                 <PostCard
                     key={post._id}
                     post={post}
+                    handleTagClick={handleTagClick}
                 />
             ))}
         </div>
@@ -20,6 +21,10 @@ const PromptCardList = ({ data }) => {
 const Feed = () => {
     const [allPosts, setAllPosts] = useState([]);
 
+    const [searchText, setSearchText] = useState("");
+    const [searchTimeout, setSearchTimeout] = useState(null);
+    const [searchedResults, setSearchedResults] = useState([]);
+
     const fetchPosts = async () => {
         const response = await fetch("/api/blogpost");
         const data = await response.json();
@@ -27,16 +32,61 @@ const Feed = () => {
         setAllPosts(data);
     };
 
+    const filterPrompts = (searchtext) => {
+        const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+        return allPosts.filter(
+            (item) =>
+                regex.test(item.creator.username) ||
+                regex.test(item.tag) ||
+                regex.test(item.prompt)
+        );
+    };
+
+    const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout);
+        setSearchText(e.target.value);
+
+        // debounce method
+        setSearchTimeout(
+            setTimeout(() => {
+                const searchResult = filterPrompts(e.target.value);
+                setSearchedResults(searchResult);
+            }, 500)
+        );
+    };
+
+    const handleTagClick = (tagName) => {
+        setSearchText(tagName);
+
+        const searchResult = filterPrompts(tagName);
+        setSearchedResults(searchResult);
+    };
+
     useEffect(() => {
         fetchPosts();
-        console.log(allPosts)
     }, []);
 
     return (
         <section className='feed'>
-            <PromptCardList
-                data={allPosts}
-            />
+            <form className='relative w-full flex_center'>
+                <input
+                    type='text'
+                    placeholder='Search for a tag or a username'
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className='search_input peer'
+                />
+            </form>
+
+            {searchText ? (
+                <PromptCardList
+                    data={searchedResults}
+                    handleTagClick={handleTagClick}
+                />
+            ) : (
+                <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+            )}
         </section>
     )
 }
